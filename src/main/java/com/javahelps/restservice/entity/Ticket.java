3bb50acd603256db526f6ab493601ec6a771d858
ReadMost <rauan.amangeldiyev@nu.edu.kz>
2019-11-15 15:23:50 +0600

    nothing changed start Ticket

--- src/main/java/com/javahelps/restservice/entity/Ticket.java
+++ src/main/java/com/javahelps/restservice/entity/Ticket.java
@@ -1,95 +1,22 @@
 package com.javahelps.restservice.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
 public class Ticket {
+
     private int id;
     private int price;
     private int document_id;
     private String lName;
     private String fName;
-    private int seat;
+    private Seats seat;
     private Station from;
     private Station to;
-    private int trainId;
-    private int carriage;
+    private Train train;
+    private Carriage carriage;
     private String status;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getDocument_id() {
-        return document_id;
-    }
-
-    public void setDocument_id(int document_id) {
-        this.document_id = document_id;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public int getSeat() {
-        return seat;
-    }
-
-    public void setSeat(int seat) {
-        this.seat = seat;
-    }
-
-    public Station getFrom() {
-        return from;
-    }
-
-    public void setFrom(Station from) {
-        this.from = from;
-    }
-
-    public Station getTo() {
-        return to;
-    }
-
-    public void setTo(Station to) {
-        this.to = to;
-    }
-
-    public int getTrainId() {
-        return trainId;
-    }
-
-    public void setTrainId(int trainId) {
-        this.trainId = trainId;
-    }
-
-    public int getCarriage() {
-        return carriage;
-    }
-
-    public void setCarriage(int carriage) {
-        this.carriage = carriage;
-    }
 }
